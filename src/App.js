04ca6880608d7a8d441eb061c6fe506cd3758895
raizik <raizik@campus.technion.ac.il>
2020-08-29 20:43:27 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Form from "./Components/From";
+import Movies from "./Components/Movies"
 
+const API_KEY = "6d4c4cb1f758aaee493e26f4efadc354"
+
+class App extends Component {
+    state = {
+        movies: []
+    }
+    getMovie = async (e) => {
+        const keyword = e.target.elements.keyword.value;
+        e.preventDefault()
+        const api_call = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&page=1&primary_release_year=${keyword}`)
+        const data = await api_call.json()
+        this.setState({ movies: data.results })
+        console.log(this.state.movies)
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Enter Year</h1>
+                </header>
+                <Form getMovie={this.getMovie}/>
+                <Movies movies={this.state.movies}/>
+            </div>
+        );
+    }
+}
 export default App;
